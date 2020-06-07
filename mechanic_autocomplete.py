a93b3a0a827f011377f1d2cbf4302720699d5679
hubert-kompanowski <hubert.kompanowski9@gmail.com>
2020-06-07 23:50:45 +0200

    mechanic autocomplete select next word add

--- mechanic_autocomplete.py
+++ mechanic_autocomplete.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 from collections import Counter
 from pynput.keyboard import Key, Listener
-
+from colorama import Fore, Style
 
 data_file = open('data.txt')
 
@@ -25,39 +25,78 @@ def find_word(char):
 
 
 INPUT = ''
+TEXT = ''
+CHOSEN_OPTION_ID = 0
+CHOSEN_OPTION = ''
+CHOSEN_OPTION_SIZE = 0
+
+
+def print_lines(to_print):
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[F")
+
+    print('     ' * 6)
+    print('     ' * 6)
+    print('-----------------------------------------------------')
+
+    for _ in range(5 - len(to_print)):
+        print('     ' * 12)
+
+    for i in to_print:
+        print(i, '     ' * 11)
 
 
 def on_press(key):
-    global INPUT
+    global INPUT, TEXT, CHOSEN_OPTION, CHOSEN_OPTION_ID, CHOSEN_OPTION_SIZE
 
     if key == Key.backspace:
         INPUT = INPUT[0: -1]
+        print()
+    elif key == Key.space:
+        TEXT = TEXT + ' ' + INPUT
+        INPUT = ''
+    elif key == Key.tab:
+        if CHOSEN_OPTION_ID < CHOSEN_OPTION_SIZE - 1:
+            CHOSEN_OPTION_ID += 1
+        else:
+            CHOSEN_OPTION_ID = 0
+    elif key == Key.enter:
+        INPUT = CHOSEN_OPTION
 
     else:
         INPUT = INPUT + key.char
 
-        sys.stdout.write("\033[F")
-    sys.stdout.write("\033[F")
-    sys.stdout.write("\033[F")
-    sys.stdout.write("\033[F")
-    sys.stdout.write("\033[F")
+    to_print = []
 
     words_found = find_word(INPUT)
-    for _ in range(4 - len(words_found)):
-        print('                             ')
+    CHOSEN_OPTION_SIZE = len(words_found)
 
-    for el in words_found:
-        print(el + '                             ')
-    print('input: ', INPUT, '           ')
+    if CHOSEN_OPTION_ID >= CHOSEN_OPTION_SIZE:
+        CHOSEN_OPTION_ID = CHOSEN_OPTION_SIZE - 1
+
+    for n, el in enumerate(words_found):
+        if n == CHOSEN_OPTION_ID:
+            to_print.append(Fore.GREEN + el + Style.RESET_ALL)
+            CHOSEN_OPTION = el
+
+        else:
+            to_print.append(el)
+
+    to_print.append('input: ' + INPUT + '           text: ' + TEXT)
+    print_lines(to_print)
 
 
 def on_release(key):
     if key == Key.esc:
-        # Stop listener
         return False
 
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    print('\n\n\n\n\n')
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    print('\n\n\n\n\n\n\n\n\n')
     listener.join()
